08d2562e5bedc72184dd22d52ff259176a477c2e
Young Soo, Lee <prevdev@gmail.com>
2013-10-16 14:01:11 +0900

    v 0.4.9 r1
    pageModel 버그 픽스

--- modules/page/PageModel.class.php
+++ modules/page/PageModel.class.php
@@ -35,14 +35,16 @@
 				->where('title', $menuTitle)
 				->find_one();
 			
-			Context::getInstance()->selectedMenu = $row->getData();
-			
-			while ($row->parent_id != NULL) {
-				$row = DBHandler::for_table('menu')
-					->where('id', $row->parent_id)
-					->find_one();
+			if ($row) {
+				Context::getInstance()->selectedMenu = $row->getData();
 				
-				array_unshift(Context::getInstance()->parentMenus, $row->getData());
+				while ($row->parent_id != NULL) {
+					$row = DBHandler::for_table('menu')
+						->where('id', $row->parent_id)
+						->find_one();
+					
+					array_unshift(Context::getInstance()->parentMenus, $row->getData());
+				}
 			}
 		}
 	}
